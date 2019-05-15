47ec7a59925e20bc9dbfdb602e1c46b68e8f4ab8
Gregor <gregor@martynus.net>
2019-05-15 13:12:58 -0700

    feat: initial version

---
+++ bin/find-hearted-contributions.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+
+const findHeartedContributions = require('..')
+
+var argv = yargs
+  .usage('Usage: $0 [options]')
+  .example('$0 --token 0123456789012345678901234567890123456789 --in https://github.com/octokit --by gr2m --since 2019-05-01')
+
+  .option('in', {
+    description: 'GitHub organization URL',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('by', {
+    description: 'GitHub username',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('since', {
+    description: 'timestamp in ISO 8601 format or GitHub issue URL, in which case since will be set to the created_at timestamp of the last comment',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('cache', {
+    description: 'Cache responses for debugging',
+    type: 'boolean',
+    default: false
+  })
+  .epilog('copyright 2019')
+  .argv
+
+findHeartedContributions(argv)
+  .then(heartedItems => {
+    console.log('\n\ndone.\n')
+
+    if (heartedItems.length === 0) {
+      console.log('No hearted items.')
+    }
+
+    console.log('hearted items:')
+
+    for (const url of heartedItems.sort()) {
+      console.log(`♥️ ${url}`)
+    }
+  })
---
+++ index.js
@@ -0,0 +1,77 @@
+module.exports = findHeartedContributions
+
+const getLastUpdateTimestamp = require('./lib/get-last-update-timestamp')
+const getRepositories = require('./lib/get-repositories')
+const getIssuesAndPullRequestsForRepository = require('./lib/get-issues-and-pull-requests-for-repository')
+const getCommentsForIssueOrPullRequestUrl = require('./lib/get-comments-for-issue-or-pull-request-url')
+const getCommitCommentsForRepository = require('./lib/get-commit-comments-for-repository')
+const getUrlIfHearted = require('./lib/get-url-if-hearted')
+
+const Octokit = require('@octokit/rest')
+  .plugin([
+    require('@octokit/plugin-throttling'),
+    require('@octokit/plugin-retry')
+  ])
+
+async function findHeartedContributions (options) {
+  const MyOctokit = options.cache ? Octokit.plugin(require('./lib/cache-plugin')) : Octokit
+  const octokit = new MyOctokit({
+    auth: options.token,
+    throttle: {
+      onAbuseLimit: (error, options) => {
+        octokit.log.error('onAbuseLimit', error, options)
+      },
+      onRateLimit: (error, options) => {
+        octokit.log.error('onRateLimit', error, options)
+      }
+    }
+  })
+
+  octokit.hook.before('request', () => process.stdout.write('.'))
+
+  if (!/^https:\/\/github\.com\/[^/]+$/.test(options.in)) {
+    throw new TypeError('"in" option must be a GitHub or url (e.g. "https://github.com/octokit"')
+  }
+
+  const owner = options.in.substr('https://github.com/'.length)
+
+  const state = {
+    octokit,
+    owner,
+    issuesAndPullRequestUrls: [],
+    commentsUrls: [],
+    comments: [],
+    ...options
+  }
+
+  try {
+    state.since = await getLastUpdateTimestamp(state)
+    state.repositories = await getRepositories(state)
+
+    for (const repositoryName of state.repositories) {
+      const issuesAndPullRequestUrls = await getIssuesAndPullRequestsForRepository(state, repositoryName)
+      state.issuesAndPullRequestUrls.push(...issuesAndPullRequestUrls)
+
+      const commentUrls = await getCommitCommentsForRepository(state, repositoryName)
+      state.commentsUrls.push(...commentUrls)
+    }
+
+    for (const url of state.issuesAndPullRequestUrls) {
+      const commentUrls = await getCommentsForIssueOrPullRequestUrl(state, url)
+      state.commentsUrls.push(...commentUrls)
+    }
+
+    const heartedItems = []
+    for (const url of [...state.issuesAndPullRequestUrls, ...state.commentsUrls]) {
+      const heartedUrl = await getUrlIfHearted(state, url)
+      if (heartedUrl) {
+        heartedItems.push(heartedUrl)
+      }
+    }
+
+    return heartedItems
+  } catch (error) {
+    octokit.log.error(error)
+    return []
+  }
+}
