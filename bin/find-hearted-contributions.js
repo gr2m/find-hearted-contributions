14e5696a452aa1653e418c10f86eca6e88f8e92e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-05 22:35:14 -0700

    refactor: make code more readable

--- bin/find-hearted-contributions.js
+++ bin/find-hearted-contributions.js
@@ -10,32 +10,34 @@ var argv = yargs
     "$0 --token 0123456789012345678901234567890123456789 --in https://github.com/octokit --by gr2m --since 2019-05-01"
   )
 
-  .option("token", {
-    description:
-      'Requires the "public_repo" scope for public repositories, "repo" scope for private repositories.',
-    demandOption: true,
-    type: "string",
-  })
-  .option("in", {
-    description: "GitHub organization URL, or the repository url",
-    demandOption: true,
-    type: "string",
-  })
-  .option("by", {
-    description: "GitHub username (or comma-separated list of usernames",
-    demandOption: true,
-    type: "string",
-  })
-  .option("since", {
-    description:
-      "timestamp in ISO 8601 format or GitHub issue URL, in which case since will be set to the created_at timestamp of the last comment",
-    demandOption: true,
-    type: "string",
-  })
-  .option("cache", {
-    description: "Cache responses for debugging",
-    type: "boolean",
-    default: false,
+  .options({
+    token: {
+      description:
+        'Requires the "public_repo" scope for public repositories, "repo" scope for private repositories.',
+      demandOption: true,
+      type: "string",
+    },
+    in: {
+      description: "GitHub organization URL, or the repository url",
+      demandOption: true,
+      type: "string",
+    },
+    by: {
+      description: "GitHub username (or comma-separated list of usernames",
+      demandOption: true,
+      type: "string",
+    },
+    since: {
+      description:
+        "timestamp in ISO 8601 format or GitHub issue URL, in which case since will be set to the created_at timestamp of the last comment",
+      demandOption: true,
+      type: "string",
+    },
+    cache: {
+      description: "Cache responses for debugging",
+      type: "boolean",
+      default: false,
+    },
   })
   .epilog("copyright 2019").argv;
 
--- index.js
+++ index.js
@@ -9,20 +9,23 @@ import getUrlIfHearted from "./lib/get-url-if-hearted.js";
 import cachePlugin from "./lib/cache-plugin.js";
 
 export default async function findHeartedContributions(options) {
+  if (!/^https:\/\/github\.com\/.+$/.test(options.in)) {
+    throw new TypeError(
+      '"in" option must be a valid GitHub repo or organization url (e.g. "https://github.com/octokit" or "https://github.com/octokit/octokit.js")'
+    );
+  }
+
   const MyOctokit = options.cache ? Octokit.plugin(cachePlugin) : Octokit;
   const octokit = new MyOctokit({
     auth: options.token,
   });
 
-  octokit.hook.before("request", () => process.stdout.write("."));
+  // log progress
+  octokit.hook.before("request", () => {
+    process.stdout.write(".");
+  });
 
-  if (!/^https:\/\/github\.com\/.+$/.test(options.in)) {
-    throw new TypeError(
-      '"in" option must be a GitHub repo url (e.g. "https://github.com/octokit"'
-    );
-  }
-
-  const owner = options.in.substr("https://github.com/".length);
+  const owner = options.in.substring("https://github.com/".length);
 
   const state = {
     octokit,
@@ -31,49 +34,51 @@ export default async function findHeartedContributions(options) {
     commentsUrls: [],
     comments: [],
     repoOnly: owner.split("/")[1],
+    repositories: [],
     ...options,
   };
 
-  try {
-    state.since = await getLastUpdateTimestamp(state);
-    if (state.repoOnly) {
-      state.owner = state.owner.split("/")[0];
-      state.repositories = [state.repoOnly];
-    } else {
-      state.repositories = await getRepositories(state);
-    }
+  // get since based on timestamp or last comment of passed URL
+  state.since = await getLastUpdateTimestamp(state);
 
-    for (const repositoryName of state.repositories) {
-      const issuesAndPullRequestUrls =
-        await getIssuesAndPullRequestsForRepository(state, repositoryName);
-      state.issuesAndPullRequestUrls.push(...issuesAndPullRequestUrls);
-
-      const commentUrls = await getCommitCommentsForRepository(
-        state,
-        repositoryName
-      );
-      state.commentsUrls.push(...commentUrls);
-    }
-
-    for (const url of state.issuesAndPullRequestUrls) {
-      const commentUrls = await getCommentsForIssueOrPullRequestUrl(state, url);
-      state.commentsUrls.push(...commentUrls);
-    }
-
-    const heartedItems = [];
-    for (const url of [
-      ...state.issuesAndPullRequestUrls,
-      ...state.commentsUrls,
-    ]) {
-      const heartedUrl = await getUrlIfHearted(state, url);
-      if (heartedUrl) {
-        heartedItems.push(heartedUrl);
-      }
-    }
-
-    return heartedItems;
-  } catch (error) {
-    octokit.log.error(error);
-    return [];
+  // normalize --in option
+  if (state.repoOnly) {
+    state.owner = state.owner.split("/")[0];
+    state.repositories = [state.repoOnly];
+  } else {
+    state.repositories = await getRepositories(state);
   }
+
+  // gather all issues, pull requests, and commit comments
+  for (const repositoryName of state.repositories) {
+    const issuesAndPullRequestUrls =
+      await getIssuesAndPullRequestsForRepository(state, repositoryName);
+    state.issuesAndPullRequestUrls.push(...issuesAndPullRequestUrls);
+
+    const commentUrls = await getCommitCommentsForRepository(
+      state,
+      repositoryName
+    );
+    state.commentsUrls.push(...commentUrls);
+  }
+
+  // get comments for issues and pull requests
+  for (const url of state.issuesAndPullRequestUrls) {
+    const commentUrls = await getCommentsForIssueOrPullRequestUrl(state, url);
+    state.commentsUrls.push(...commentUrls);
+  }
+
+  // iterate through all issues, pull requests, and comments and check if they are hearted
+  const heartedItems = [];
+  for (const url of [
+    ...state.issuesAndPullRequestUrls,
+    ...state.commentsUrls,
+  ]) {
+    const heartedUrl = await getUrlIfHearted(state, url);
+    if (heartedUrl) {
+      heartedItems.push(heartedUrl);
+    }
+  }
+
+  return heartedItems;
 }
